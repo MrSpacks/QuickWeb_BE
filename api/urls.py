cf1ce2a191c96c1a33911190c7a90938b48a701f
Mr. Spacks <94480894+MrSpacks@users.noreply.github.com>
2025-07-27 17:43:56 +0200

    fixed bugs and optimization

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BusinessCardViewSet, SocialLinkViewSet, VisitStatsViewSet, card_detail, register_user, login_user
+from .views import BusinessCardViewSet, SocialLinkViewSet, VisitStatsViewSet, register_user, login_user
 
 router = DefaultRouter()
 router.register(r'cards', BusinessCardViewSet, basename='cards')
@@ -11,5 +11,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
-    path('<slug:slug>/', card_detail, name='card_detail'),
 ]
--- quick_web/urls.py
+++ quick_web/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from api.views import card_detail  # Импортируем card_detail напрямую
 
 urlpatterns = [
-    # Админка
     path('admin/', admin.site.urls),
-    # API (для DRF)
-    path('api/', include('api.urls')),  # Подключаем urls.py приложения
-    # Публичные визитки
-    path('<slug:slug>/', include('api.urls')),  # Для визиток вида example.com/cardname
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Для медиафайлов
+    path('api/', include('api.urls')),  # Для API
+    path('<slug:slug>/', card_detail, name='card_detail'),  # Публичные визитки
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
